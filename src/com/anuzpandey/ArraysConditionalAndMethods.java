209f6b0b6409d561f211f058cf9bd13ae2163696
Anuz Pandey <anuzbvbmaniac123@gmail.com>
2023-03-15 00:23:27 +0545

    :zap: chore: Array Conditional And Methods Question 3 complete.

--- src/com/anuzpandey/ArraysConditionalAndMethods.java
+++ src/com/anuzpandey/ArraysConditionalAndMethods.java
@@ -5,8 +5,9 @@ import java.util.Arrays;
 public class ArraysConditionalAndMethods {
 
     public static void main(String[] args) {
-        exerciseOne("AppleMando");
+        exerciseOne("Anuj");
         exerciseTwo();
+        exerciseThree();
     }
 
 
@@ -16,12 +17,14 @@ public class ArraysConditionalAndMethods {
      * Output should be reversed input
      */
     public static void exerciseOne(String input) {
+        System.out.println(" ");
         System.out.println("----- Exercise One -----");
-        String reverse = "";
+        String reverseString = "";
         for (int i = input.length() - 1; i >= 0; i--) {
-            reverse = reverse + input.charAt(i);
+            reverseString += input.charAt(i);
         }
-        System.out.println(reverse);
+
+        System.out.println(reverseString);
     }
 
 
@@ -31,17 +34,51 @@ public class ArraysConditionalAndMethods {
      * Write a method that transforms input into: Amigoscode
      */
     public static void exerciseTwo() {
+        System.out.println(" ");
         System.out.println("----- Exercise Two -----");
         String input = "   amig os cod e  ";
+
         char[] characters = input.toCharArray();
         String result = "";
 
-        for (int i = 0; i <= input.length() - 1; i++) {
+        for (int i = 0; i <= characters.length - 1; i++) {
             if (!String.valueOf(characters[i]).equals(" ")) {
-                result = result + characters[i];
+                result += characters[i];
             }
         }
 
-        System.out.println(result.substring(0, 1).toUpperCase() + result.substring(1));
+        result = result.substring(0, 1).toUpperCase() + result.substring(1);
+
+        System.out.println(result);
+    }
+
+
+    /**
+     * Write a method that finds the longest string in any given array.
+     * i.e. [] -> ""
+     * i.e. ["hello", "ola", "bye", "ciao"] -> hello
+     * i.e. ["hello", "hello", "ola", "bye", "ciao"] -> hello
+     * i.e. ["hello", "bingo", "ola", "bye", "ciao"] -> hello, bingo
+     */
+    public static void exerciseThree() {
+        System.out.println(" ");
+        System.out.println("----- Exercise Three -----");
+
+        String[] input = {"hello", "bingo", "ola", "bye", "ciao", "namaste", "tamaste"};
+
+        String results = "";
+        String temporaryPlaceholder = "";
+
+        for(int i = 0; i <= input.length - 1; i++) {
+            if (input[i].length() > temporaryPlaceholder.length()) {
+                temporaryPlaceholder = input[i];
+                results = temporaryPlaceholder + ", ";
+            } else if (input[i].length() == temporaryPlaceholder.length()) {
+                temporaryPlaceholder = input[i];
+                results += temporaryPlaceholder + ", ";
+            }
+        }
+
+        System.out.println(results.substring(0, results.length() - 2));
     }
 }
